493853a217a37a6b2d0aa73a69aa9bd0fe460237
mkursaterbek <mehmetkursaterbek@hotmail.com>
2022-09-13 10:02:07 +0300

    add new buttons from js

--- script.js
+++ script.js
@@ -1,25 +1,59 @@
-let colors = ["red", "blue", "yellow", "green", "grey"];
-
-const btns = document.querySelectorAll(".button");
 const monitor = document.querySelector(".monitor");
 let count = 0;
 
-btns.forEach(function (btn) {
-  btn.addEventListener("click", function (e) {
-    const styles = e.currentTarget.classList;
-    document.querySelector("body").style.background = // bu işlemi dışarıda yapıp burada nasıl tanımlardık ?
-      colors[Math.floor(Math.random() * colors.length)];
-    if (styles.contains("increase")) {
-      count++;
-    } else if (styles.contains("decrease")) {
-      count--;
-    } else {
-      count = 0;
-      document.querySelector("body").style.background = "none"; // bu işlemi dışarıda yapıp burada nasıl tanımlardık ?
-    }
-    monitor.textContent = count;
-  });
+const btnDec = document.createElement("button");
+btnDec.innerHTML = "decrease";
+document.getElementById("buttons").appendChild(btnDec);
+btnDec.setAttribute("id", "btnDec");
+
+const btnRes = document.createElement("button");
+btnRes.innerHTML = "Reset";
+document.getElementById("buttons").appendChild(btnRes);
+btnRes.setAttribute("id", "btnRes");
+
+const btnInc = document.createElement("button");
+btnInc.innerHTML = "Increase";
+document.getElementById("buttons").appendChild(btnInc);
+btnInc.setAttribute("id", "btnInc");
+
+btnDec.addEventListener("click", function () {
+  count--;
+  document.querySelector("#monitor").innerHTML = count;
+  document.querySelector("body").style.background = "red";
 });
+btnRes.addEventListener("click", function () {
+  count = 0;
+  document.querySelector("#monitor").innerHTML = count;
+  document.querySelector("body").style.background = "none";
+});
+btnInc.addEventListener("click", function () {
+  count++;
+  document.querySelector("#monitor").innerHTML = count;
+  document.querySelector("body").style.background = "green";
+});
+
+// let colors = ["red", "blue", "yellow", "green", "grey"];
+
+// const btns = document.querySelectorAll(".button");
+// const monitor = document.querySelector(".monitor");
+// let count = 0;
+
+// btns.forEach(function (btn) {
+//   btn.addEventListener("click", function (e) {
+//     const styles = e.currentTarget.classList;
+//     document.querySelector("body").style.background = // bu işlemi dışarıda yapıp burada nasıl tanımlardık ?
+//       colors[Math.floor(Math.random() * colors.length)];
+//     if (styles.contains("increase")) {
+//       count++;
+//     } else if (styles.contains("decrease")) {
+//       count--;
+//     } else {
+//       count = 0;
+//       document.querySelector("body").style.background = "none"; // bu işlemi dışarıda yapıp burada nasıl tanımlardık ?
+//     }
+//     monitor.textContent = count;
+//   });
+// });
 
 // ******************************************* SORULAR *******************************************
 // dışarıda yeni bir fonksiyon tanımlayıp forEach olan funcksiyonun içinde tanımlayabilir miyidik?
